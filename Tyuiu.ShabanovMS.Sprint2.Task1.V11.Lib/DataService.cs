2f2601e9061826b9fd703cfd06d17fd3218b529d
Tyuiu.ShabanovMS@outlook.com <shabanov2014@bk.ru>
2023-10-12 20:39:00 +0500

    Add Task1

---
+++ Tyuiu.ShabanovMS.Sprint2.Task1.V11.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+//(False, False, True, False, True, False), при a = 145, b = 156, c = 142, d = 117
+// |, &, ||, &&, !, ^,
+namespace Tyuiu.ShabanovMS.Sprint2.Task1.V11.Lib
+{
+    public class DataService : ISprint2Task1V11
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+            res[0] = (a > b) | (c < d);  //false
+            res[1] = (a > b) & (c < d);  //false
+            res[2] = (a < b) || (c < d); //true
+            res[3] = (a > b) && (c < d); //false
+            res[4] = !(a > b);           //true
+            res[5] = (a < b) ^ (c > d);  //false
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.ShabanovMS.Sprint2.Task1.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShabanovMS.Sprint2.Task1.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShabanovMS.Sprint2.Task1.V11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("de62f4e6-2aaf-4dd4-8056-dc767c961eab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
