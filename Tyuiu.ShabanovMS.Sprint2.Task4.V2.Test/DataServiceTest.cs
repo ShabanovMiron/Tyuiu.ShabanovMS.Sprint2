a5f1c9da7b3823283c5389c86f0cb13ffe5c449a
Tyuiu.ShabanovMS@outlook.com <shabanov2014@bk.ru>
2023-10-13 23:25:59 +0500

    Add Task5

---
+++ Tyuiu.ShabanovMS.Sprint2.Task4.V2.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ShabanovMS.Sprint2.Task4.V2.Lib;
+namespace Tyuiu.ShabanovMS.Sprint2.Task4.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 1;
+            double res = ds.Calculate(x, y);
+            double wait = 21;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.ShabanovMS.Sprint2.Task4.V2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShabanovMS.Sprint2.Task4.V2.Lib;
+namespace Tyuiu.ShabanovMS.Sprint2.Task4.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Шабанов М.С. | АСОиУб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт 2                                                                *");
+            Console.WriteLine("* Тема: Вложенные операторы if - else                                     *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("*Выполнил: Шабанов Мирон Сергеевич | АСОиУб-23-1                          *");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение                *");
+            Console.WriteLine("* с использованием тернарного оператора  где пользователь вводит          *");
+            Console.WriteLine("* значение переменных X, Y с клавиатуры.                                  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            DataService ds = new DataService();
+            double z = ds.Calculate(x,y);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Значение Zf = " + z);
+            Console.ReadKey();
+        }
+    }
+}
