571dd409bf7637bce1521368315a45b6bdda3247
Tyuiu.ShabanovMS@outlook.com <shabanov2014@bk.ru>
2023-10-15 20:00:45 +0500

    Add Task6

---
+++ Tyuiu.ShabanovMS.Sprint2.Task6.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShabanovMS.Sprint2.Task6.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShabanovMS.Sprint2.Task6.V11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a1f1d1bd-cb92-4e28-a8b4-6113318afde6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
